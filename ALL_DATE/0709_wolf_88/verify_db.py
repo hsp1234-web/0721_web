690b0010e72e1f92387e8fbbab646eedf3eb6620
hsp-1234 <3514217@gmail.com>
2025-08-02 02:12:26 +0800

    Initial commit: Cleaned and structured project based on v17 architecture

--- ALL_DATE/0709_wolf_88/verify_db.py
+++
@@ -1,6 +0,0 @@
-import duckdb
-
-con = duckdb.connect('data/analytics_warehouse/factors.duckdb')
-res = con.execute("SELECT ticker, COUNT(*) FROM bond_specific_factors GROUP BY ticker").fetchall()
-print(res)
-con.close()
